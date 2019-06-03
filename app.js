d74e684dc1b88b82e6d547d6d226e5eee74bf875
Marc <marcjfj@gmail.com>
2019-06-03 09:24:52 -0500

    added servo smoothing

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ pwm = new Pca9685Driver(options, function(err) {
 let servoPL = 1500;
 let servoMin = 600;
 let servoMax = 2400;
-let servoMove = servoPL;
+let servoMove = 1500;
 app.get('/servo-plus', (req, res) => {
     if (servoPL < servoMax){
         while (servoMove < (servoPL + 100)) {
